d7f66695bb41b6033cb78cbf4c256a96eff9c629
alberthgabriel5 <alberthgabriel5@hotmail.com>
2017-02-22 04:52:12 -0600

    documentacion22-02-2017

--- Data/purchaseData.php
+++ Data/purchaseData.php
@@ -8,34 +8,6 @@ include_once '../../Domain/purchase.php';
 class purchaseData extends Data
 {
     
-    /*
-     * Función que permite el registro de las compras a los provedores en la base de datos
-     * primero consulta el id para crear un consecutivo y luego registra el nuevo
-     * @param type $purchase
-     * @return boolean
-     */
-    function insertPurchase($purchase) {
-        $conn = new mysqli($this->server, $this->user, $this->password, $this->db);
-        $conn->set_charset('utf8');
-//Se consulta por el ultimo id registrado para generar el consecutivo
-        $resultID = mysqli_query($conn, "select idPurchases from tbpurchasingsupplier order by idPurchases desc limit 1");
-        $row = mysqli_fetch_array($resultID);
-        if (sizeof($row) >= 1) {
-            $id = $row['idPurchases'] + 1;
-        } else {
-            $id = 1;
-        }
-        $queryInsert = mysqli_query($conn, "insert into tbpurchasingsupplier values (" .
-                $id . ",'" .
-                $purchase->getIdSupplier() . "','" .
-                $purchase->getDatePurchase() . "','" .
-                $purchase->getDescriptionPurchase() . "','" .
-                $purchase->getIdProduct() . "','" .
-                $purchase->getTotalPurchase() ."',1);");        
-        mysqli_close($conn);
-        return $queryInsert;
-        
-    }//fin function insertsupplier
    /**
      * Función que permite la obtención de todos los registros de 
      * provedores de la base de datos
